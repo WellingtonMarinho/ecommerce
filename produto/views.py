f619d3ef0a334459465206b942005c2c2c795875
WellingtonMarinho <wellington.gnr@hotmail.com>
2020-12-10 16:34:27 -0300

    adc urls inicias e base das class based views

--- produto/views.py
+++ produto/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views import View
+from django.http import HttpResponse
 
-# Create your views here.
+
+class ListaProdutos(ListView):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Listar produtos')
+
+
+class DetalheProdutos(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Detalhe Produtos')
+
+
+class AdcionarAoCarrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Adicionar ao carrinho')
+
+
+class RemoverDoCarrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Remover do carrinho')
+
+
+class Carrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Carrinho')
+
+
+class Finalizar(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Finalizar')
